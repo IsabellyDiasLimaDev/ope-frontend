750accc3cbfbb59ae24114258afdb2a90952b8a6
guilhermeabel99 <gpeuvis@gmail.com>
2021-11-04 22:19:23 -0300

    refactoring css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,15 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="index.html">
+            <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="index.html">
               <img src="https://cdn.discordapp.com/attachments/746027270845759568/845342039252009020/logoexten.png" width="220"
-                height="40" class="d-inline-block align-top" alt=""/></a>
-            <a class="nav-item nav-link active" href="/index">Inicio</a>
-            <a class="nav-item nav-link" href="/cadastromateriais">Materiais / Serviços</a>
-            <a class="nav-item nav-link" href="#">Auxiliares</a>
-            <a class="nav-item nav-link" href="/orcamento">Orçamento</a>
-            <a class="nav-item nav-link" href="/logout">Sair</a>
+                height="40" className="d-inline-block align-top" alt=""/></a>
+            <a className="nav-item nav-link active" href="/index">Inicio</a>
+            <a className="nav-item nav-link" href="/cadastromateriais">Materiais / Serviços</a>
+            <a className="nav-item nav-link" href="#">Auxiliares</a>
+            <a className="nav-item nav-link" href="/orcamento">Orçamento</a>
+            <a className="nav-item nav-link" href="/logout">Sair</a>
           </nav>
         );
     }
