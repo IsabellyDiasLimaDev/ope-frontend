6c12e0abf79503dfdee218c42a4432108762bf85
LucasRdSilva <lucasrsilva42@gmail.com>
2021-11-10 22:20:39 -0300

    Ajuste rota e css Materiais/Servicos, Auxiliares
    Co-authored-by: Luiz Ortega <luizortega27@users.noreply.github.com>

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,9 +10,9 @@ class Navbar extends Component {
             <a className="navbar-brand" href="index.html">
               <img src="https://cdn.discordapp.com/attachments/746027270845759568/845342039252009020/logoexten.png" width="220"
                 height="40" className="d-inline-block align-top" alt=""/></a>
-            <a className="nav-item nav-link active" href="/index">Inicio</a>
-            <a className="nav-item nav-link" href="/cadastromateriais">Materiais / Serviços</a>
-            <a className="nav-item nav-link" href="#">Auxiliares</a>
+            <a className="nav-item nav-link active" href="#">Inicio</a>
+            <a className="nav-item nav-link" href="/materiais">Materiais / Serviços</a>
+            <a className="nav-item nav-link" href="/auxiliares">Auxiliares</a>
             <a className="nav-item nav-link" href="/orcamento">Orçamento</a>
             <a className="nav-item nav-link" href="/logout">Sair</a>
           </nav>
